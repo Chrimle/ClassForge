1153ebc13876f4f4852cccec229011a7cf439109
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-07-15 14:27:36 +0200

    Refactor Tests

--- src/test/java/io/github/chrimle/classforge/ClassBuilderTest.java
+++ src/test/java/io/github/chrimle/classforge/ClassBuilderTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import io.github.chrimle.classforge.test.utils.DynamicClassLoader;
 import io.github.chrimle.classforge.test.utils.JavaSourceCompiler;
+import java.io.IOException;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,15 +17,27 @@ class ClassBuilderTest {
   public static final String PACKAGE_NAME = "io.github.chrimle.classforge";
   public static final String PACKAGE_NAME_DIRECTORY = PACKAGE_NAME.replace(".", "/");
 
-  @Test
-  void test() throws Exception {
-    final Path outputDir = Path.of(ABSOLUTE_PATH_PREFIX);
-    new ClassBuilder(outputDir.toString(), PACKAGE_NAME, "ClassName").build();
+  static Class<?> compileAndLoadClass(final String className) throws Exception {
+    compileClass(className);
+    return loadClass(className);
+  }
 
-    JavaSourceCompiler.compile(outputDir.resolve(PACKAGE_NAME_DIRECTORY + "/ClassName.java"));
+  static void compileClass(final String className) throws IOException {
+    JavaSourceCompiler.compile(
+        Path.of(ABSOLUTE_PATH_PREFIX).resolve(PACKAGE_NAME_DIRECTORY + "/" + className + ".java"));
+  }
 
-    final Class<?> clazz = DynamicClassLoader.loadClass(outputDir, PACKAGE_NAME + ".ClassName");
-    assertNotNull(clazz);
+  static Class<?> loadClass(final String className) throws Exception {
+    return DynamicClassLoader.loadClass(
+        Path.of(ABSOLUTE_PATH_PREFIX), String.join(".", PACKAGE_NAME, className));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"ClassName"})
+  void testCreatingClass(final String className) throws Exception {
+    new ClassBuilder(ABSOLUTE_PATH_PREFIX, PACKAGE_NAME, className).build();
+
+    assertNotNull(compileAndLoadClass(className));
   }
 
   @Nested
@@ -39,8 +51,7 @@ class ClassBuilderTest {
               IllegalArgumentException.class,
               () ->
                   new ClassBuilder(
-                          absolutePathPrefix, PACKAGE_NAME, "TestInvalidAbsolutePathPrefix")
-                      .build());
+                          absolutePathPrefix, PACKAGE_NAME, "TestInvalidAbsolutePathPrefix"));
       assertEquals("`absolutePathPrefix` MUST NOT be `null`!", exception.getMessage());
     }
   }
