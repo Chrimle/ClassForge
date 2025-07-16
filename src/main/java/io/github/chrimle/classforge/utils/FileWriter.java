86ae310f3d3ac59e7d52a976ea5084990730984f
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-07-16 15:53:55 +0200

    Add Missing JavaDocs

--- src/main/java/io/github/chrimle/classforge/utils/FileWriter.java
+++ src/main/java/io/github/chrimle/classforge/utils/FileWriter.java
@@ -4,8 +4,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
+/**
+ * Writes content to files - creating directories and files if needed.
+ *
+ * @since 0.1.0
+ * @author Chrimle
+ */
 public class FileWriter {
 
+  /**
+   * Writes the {@code content} to a file at {@code directory} and the {@code package}-name from the
+   * {@code fullyQualifiedClassName}.
+   *
+   * @param directory of the file.
+   * @param fullyQualifiedClassName of the class.
+   * @param content of the file.
+   * @since 0.1.0
+   */
   public static void writeToFile(
       final String directory, final String fullyQualifiedClassName, final String content) {
     writeToFile(
@@ -13,6 +28,14 @@ public class FileWriter {
         content);
   }
 
+  /**
+   * Writes the {@code content} to a file at {@code filePath}. Creates the directory and file if
+   * needed.
+   *
+   * @param filePath of the file.
+   * @param content of the file.
+   * @since 0.1.0
+   */
   public static void writeToFile(final Path filePath, final String content) {
     try {
       Files.createDirectories(filePath.getParent());
