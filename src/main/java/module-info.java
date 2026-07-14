c6505b91c4a6e1650c8a5e4e0f57f82bc5f2d71d
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2026-07-14 12:06:46 +0200

    Export `io.github.chrimle.classforge` Module (#152)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/** Forge Java Classes! */
+module io.github.chrimle.classforge {
+  exports io.github.chrimle.classforge;
+  exports io.github.chrimle.classforge.enums;
+
+  // Requires (non-static)
+  requires io.github.chrimle.exceptionfactory;
+  requires io.github.chrimle.semver;
+  requires java.compiler;
+
+  // Requires (static)
+  requires static org.apiguardian.api;
+  requires static org.jetbrains.annotations;
+}
